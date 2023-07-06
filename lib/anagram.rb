c8958ee03ecbc2411b9f5ecd160fec6823f5d26b
cynthia <cynthiakagz@gmail.com>
2023-07-06 22:17:05 +0300

    completed

--- lib/anagram.rb
+++ lib/anagram.rb
@@ -1,1 +1,21 @@
 # Your code goes here!
+class Anagram
+    def initialize(word)
+      @word = word.downcase
+    end
+  
+    def match(words)
+      words.select { |w| anagram?(w.downcase) }
+    end
+  
+    private
+  
+    def anagram?(other_word)
+      return false if @word == other_word
+      sort_characters(@word) == sort_characters(other_word)
+    end
+  
+    def sort_characters(word)
+      word.chars.sort.join
+    end
+  end
